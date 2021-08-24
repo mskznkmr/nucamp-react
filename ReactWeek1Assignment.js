6ff1d6302db4c1f98ce3d9fbaba2643837b9f75a
Masakazu Nakamura <masa@masa.us>
2021-08-23 23:19:18 -0700

    installed yarn

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -8,6 +8,7 @@ class Student {
 }
 
 class Bootcamp {
+    // if students array is not passed in, then initialize it to an empty array
     constructor(name, level, students = []) {
         this.name = name;
         this.level = level;
@@ -26,40 +27,24 @@ class Bootcamp {
             this.students.push(student);
             console.log(`Registering ${student.email} to the bootcamp ${student.community}.`);
         }
-      
-        // console.log(this.students);
-        return this.students;
-    }
-
-    registerStudent2(student) {
-        console.log("Inside registerStudent method.");
-        // Creating a temp array that includes a student object that has the same email address as the email address of passed-in student object
-        const regStudentCheck = this.students.filter(s => s.email === student.email);
-
-        // If regStudentCheck is empty, it means there is no matching email address 
-        if(regStudentCheck.length === 0){
-            this.students.push(student);
-            console.log(`Registering ${student.email} to the bootcamp ${student.community}.`);
+        else {
+            console.log(`${student.email} already exists.`);
         }
       
         // console.log(this.students);
         return this.students;
     }
-    
-
 }
 
-// console.log("Starting scripst.");
-
-// const masa = new Student("Masa", "masa@masa.us", "Web Dev Fundamentals");
-// const julia = new Student("julia", "julia@masa.us", "Web Dev Fundamentals");
+// const test2 = new Student("Student 2", "student2@email.com", "Web Dev Fundamentals");
+// const test1 = new Student("Student 1", "student1@email.com", "Web Dev Fundamentals");
 
 // const nucamp = new Bootcamp("NuCamp", "1");
 
-// nucamp.registerStudent(masa);
-// nucamp.registerStudent(masa);
-// nucamp.registerStudent(julia);
-// console.log(nucamp.registerStudent(julia));
+// nucamp.registerStudent(test2);
+// nucamp.registerStudent(test2);
+// nucamp.registerStudent(test1);
+// console.log(nucamp.registerStudent(test1));
 
 
 // console.log("Ending scripst.");
