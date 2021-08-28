419aec84ab61af8f23ade9e19fc22262d4823b6e
Masakazu Nakamura <masa@masa.us>
2021-08-28 15:26:07 -0700

    Week 2 Workshop finished

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ class CampsiteInfo extends Component {
             return (
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    <div>{comments.map(comment => <div key={comment.key}><p>{comment.text}<br />-- {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                    <div>{comments.map(comment => <div key={comment.id}><p>{comment.text}<br />-- {comment.author} - {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
 </div>)}</div>
                 </div>
             ); 
