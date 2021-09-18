9aa789dffbf879c7ac1d709d4cd23974b9044b83
Masakazu Nakamura <masa@masa.us>
2021-09-17 21:52:45 -0700

    Fetch post comment

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -25,7 +25,7 @@ function RenderCampsite({campsite}) {
 }
 
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if(comments) {
         return (
             <div className="col-md-5 m-1">
@@ -39,7 +39,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                             </div>
                     );
                 })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -82,7 +82,7 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} addComment={props.addComment} campsiteId={props.campsite.id} />
+                    <RenderComments comments={props.comments} postComment={props.postComment} campsiteId={props.campsite.id} />
                 </div>
             </div>
         );
@@ -116,7 +116,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
 
     }
 
